379c2c2e95e5440d801bae37ccf8ed96e8ec719c
Alvin Gille <alvin.gille@elev.ga.ntig.se>
2024-11-12 10:51:11 +0100

    5

--- nasär/Program.cs
+++ nasär/Program.cs
@@ -18,10 +18,18 @@ using System.Runtime.Intrinsics.Arm;
 //     Console.WriteLine("Wrong username or password");
 // }
 
-for (int i = 0; i < 32; i++)
+// for (int i = 0; i < 32; i++)
 
+// {
+//     Console.WriteLine("Hello, World!");
+// }
+
+string name = "";
+
+while (name != "Micke")
 {
-    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Skriv ditt namn:");
+    name = Console.ReadLine();
 }
 
 
